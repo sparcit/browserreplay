197465f2169fe61251e16e4b4d655e5a4ead29f8
Aby Kurian <kurianaby@gmail.com>
2026-04-04 22:27:18 +0100

    Fixed the local agent invalid result error

--- src/local-agent.ts
+++ src/local-agent.ts
@@ -201,7 +201,7 @@ export class LocalAgentClient implements AgentClient {
                 },
               },
             },
-            required: ["result"],
+            required: ["result", "evidence"],
           },
         },
       },
